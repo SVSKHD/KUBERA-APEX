ed98e0c5ae11d0e2e87bda40fe27ed86b4d173de
Madhurinagashetty <138663908+Madhurinagashetty@users.noreply.github.com>
2024-04-15 06:56:27 +0530

    logic enhance and separate functions

--- trade_logic.py
+++ trade_logic.py
@@ -74,14 +74,8 @@ def check_price_difference(symbol):
         print(f"Trade placed: {direction} at {trade_price}")
 
     loss_percentage = print_open_positions()
-    if last_trade["price"] is not None:
-        price_diff = (current_price - last_trade["price"]) / pip_size
-        if (last_trade["direction"] == 'BUY' and price_diff <= -opposite_pip_move) or \
-                (last_trade["direction"] == 'SELL' and price_diff >= opposite_pip_move) or \
-                loss_percentage <= -1:
-            close_all_trades()
-            last_trade.update({"price": None, "direction": None})
-            print(f"All positions closed due to a {opposite_pip_move} pip move against the last trade or a total loss exceeding 1%.")
+    check_and_close_trades(current_price)
+    check_loss_and_close_trades(loss_percentage)
 
     if last_trade["price"] is None or abs(last_trade["price"] - current_price) >= pip_difference * pip_size:
         if high_diff <= pip_difference:
@@ -91,4 +85,21 @@ def check_price_difference(symbol):
             print(f"Current price of {symbol} is close to the lowest price over the last {CANDLE_DATA} candles. Considering to BUY.")
             handle_trade('BUY', current_price)
         else:
-            print(f"No significant price difference observed for {symbol} over the last {CANDLE_DATA} candles. Current Price: {current_price}, Lowest: {low}, Highest: {high} pip-difference low - {low_diff} high-diff - {high_diff}")
+            print(f"No significant price difference observed for {symbol} over the last {CANDLE_DATA} candles. Current Price: {current_price}, Lowest: {low}, Highest: {high}, pip-difference low - {low_diff}, high-diff - {high_diff}")
+
+# These functions are called to check and close trades based on specific conditions
+def check_and_close_trades(current_price):
+    global last_trade
+    if last_trade["price"] is not None:
+        price_diff = (current_price - last_trade["price"]) / pip_size
+        if (last_trade["direction"] == 'BUY' and price_diff <= -opposite_pip_move) or \
+           (last_trade["direction"] == 'SELL' and price_diff >= opposite_pip_move):
+            close_all_trades()
+            last_trade.update({"price": None, "direction": None})
+            print(f"All positions closed due to a {opposite_pip_move} pip move against the last trade.")
+
+def check_loss_and_close_trades(loss_percentage):
+    if loss_percentage <= -1:
+        close_all_trades()
+        last_trade.update({"price": None, "direction": None})
+        print(f"All positions closed due to a total loss exceeding 1%.")
