bc7da8299fd8e566005d84777cd66fedb75b68d4
SVSKHD <8svskhd@gmail.com>
2024-05-12 18:12:12 +0530

    close_all_orders

--- forex/reusables.py
+++ forex/reusables.py
@@ -64,36 +64,43 @@ def get_open_orders(open_params=None):
 
 # order management------------------------
 def close_all_trades():
-    # Retrieve information about all open positions
     positions = mt5.positions_get()
     if positions is None:
         print("No positions found, error code =", mt5.last_error())
-    elif len(positions) > 0:
-        for position in positions:
-            symbol = position.symbol
-            volume = position.volume
-            position_id = position.ticket
-            trade_type = mt5.ORDER_TYPE_SELL if position.type == mt5.ORDER_TYPE_BUY else mt5.ORDER_TYPE_BUY
+        return False
 
-            # Prepare the request for closing the position
-            close_request = {
-                "action": mt5.TRADE_ACTION_DEAL,
-                "symbol": symbol,
-                "volume": volume,
-                "type": trade_type,
-                "position": position_id,
-                "magic": 0,
-                "comment": "Closed by script",
-                "type_time": mt5.ORDER_TIME_GTC,
-                "type_filling": mt5.ORDER_FILLING_FOK,
-            }
+    all_closed = True
+    global balance  # Ensure balance is accessible globally
 
-            # Send the trade request
-            result = mt5.order_send(close_request)
-            if result.retcode != mt5.TRADE_RETCODE_DONE:
-                print(f"Failed to close position {position_id}: {result}")
-            else:
-                print(f"reusabale.py : Position {position_id} closed successfully.")
+    for position in positions:
+        symbol = position.symbol
+        volume = position.volume
+        position_id = position.ticket
+        trade_type = mt5.ORDER_TYPE_SELL if position.type == mt4.ORDER_TYPE_BUY else mt5.ORDER_TYPE_BUY
+
+        close_request = {
+            "action": mt5.TRADE_ACTION_DEAL,
+            "symbol": symbol,
+            "volume": volume,
+            "type": trade_type,
+            "position": position_id,
+            "magic": 0,
+            "comment": "Closed by script",
+            "type_time": mt5.ORDER_TIME_GTC,
+            "type_filling": mt5.ORDER_FILLING_FOK,
+        }
+
+        result = mt5.order_send(close_request)
+        if result.retcode != mt5.TRADE_RETCODE_DONE:
+            print(f"Failed to close position {position_id}: {result}")
+            balance = get_account_balance()  # Update balance after each failed operation
+            all_closed = False
+        else:
+            print(f"reusables.py: Position {position_id} closed successfully.")
+            balance = get_account_balance()  # Update balance after each successful operation
+
+    return all_closed
+
 
 
 # profit and loss trades calculations------
